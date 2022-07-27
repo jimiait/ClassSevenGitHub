2268abfa64502a0f692c8346f060e10b019fa2a7
jimiait <106033479+jimiait@users.noreply.github.com>
2022-07-27 03:02:45 -0500

    4th commit

--- src/main/java/git/GitSbranch2.java
+++ src/main/java/git/GitSbranch2.java
@@ -6,6 +6,13 @@ public class GitSbranch2 {
 		
 		System.out.println("git Sbranch2 test");
 		System.out.println("git checkout Sbranch2");
+		System.out.println("Lead request to modify some code");
+		System.out.println("I have to making sure before I work on the code I am on right BRANCH");
+		System.out.println("git Status");
+		System.out.println("git add .");
+		System.out.println("git commit -m 4th commit");
+		System.out.println("git push -u origin Sbranch1");
+		
 	}
 
 }
