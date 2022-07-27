ccd42be9b2e5b4b2edbc7bccf14447d95529277b
jimiait <106033479+jimiait@users.noreply.github.com>
2022-07-27 02:51:56 -0500

    3rd commit

---
+++ src/main/java/git/GitSbranch2.java
@@ -0,0 +1,11 @@
+package git;
+
+public class GitSbranch2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("git Sbranch2 test");
+		System.out.println("git checkout Sbranch2");
+	}
+
+}
